d64c888b6d5e75443a6acbd895b65cc672f5cc7f
Fajar Romadhon <fajar599@gmail.com>
2022-10-02 17:31:33 +0700

    perbaikan pada fitur dark mode

--- app/src/main/java/com/example/githubusersub2/SplashScreen.kt
+++ app/src/main/java/com/example/githubusersub2/SplashScreen.kt
@@ -1,12 +1,10 @@
-package com.example.githubusersub2.UImode
+package com.example.githubusersub2
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import com.example.githubusersub2.MainActivity
-import com.example.githubusersub2.R
 
 class SplashScreen : AppCompatActivity() {
 
